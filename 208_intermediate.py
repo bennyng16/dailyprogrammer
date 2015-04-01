c99b94b4f71b509e6e93d69e13764a47a0bae3ca
Benny <bennyng@umich.edu>
2015-04-01 11:00:08 -0400

    I did stuff

--- 208_intermediate.py
+++ 208_intermediate.py
@@ -18,19 +18,30 @@ try:
 	height = int (ins.split ()[1])
 
 	# Type of Gradient #
-
-	ins = raw_input ()
+	ins = raw_input ("Enter a radial gradient: ")
+	ins = raw_input ("Enter either 'radial x y r' | 'linear x1 y1 x2 y2':)
 	s = ins.split ()[0]
 	if s == "radial":
 		# parse x y r
-		s = ''
+		x = ins.split ()[1]
+		y = ins.split ()[2]
+		r = ins.split ()[3]
+		print "You selected Radial Gradient"
+
+
 	elif s == "linear":
 		# parse x1 y1 x2 y2
-		s = ''
+		x1 = ins.split ()[1]
+		y1 = ins.split ()[2]
+		x2 = ins.split ()[3]
+		y2 = ins.split ()[4]
+		print "You selected Linear Gradient"
+
 	else:
 		raise ("Invalid input.")
 	matrix = init_matrix (width, height)
 
+	matrix[0][0] = grad_scale[4]
 	print_matrix (matrix)
 
 except:
